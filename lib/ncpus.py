f381ca04a6882acffdaf199438d3211ca01bab35
Luis Pedro Coelho <luis@luispedro.org>
2017-10-02 15:11:44 +0200

    BUG Fix detection of # of CPUs

--- lib/ncpus.py
+++ lib/ncpus.py
@@ -1,15 +1,16 @@
 def get_ncpus():
     from os import environ
-    if 'OMP_NUM_THREADS' in environ:
-        return int(environ['OMP_NUM_THREADS'])
-    for ev in ['Q_CORE', 'LSB_MCPU_HOSTS']:
+    for ev in ['OMP_NUM_THREADS', 'Q_CORES', 'Q_CORE']:
+        if ev in environ:
+            return int(environ[ev].strip())
+    for ev in ['LSB_MCPU_HOSTS']:
         if ev in environ:
             break
     else:
         return 1
     tokens = environ[ev].strip().split()
     if len(tokens) > 2:
-        raise SystemError("Cannot handle this type of environment ({}='{}')".format(ev, environment[ev]))
+        raise SystemError("Cannot handle this type of environment ({}='{}')".format(ev, environ[ev]))
     return int(tokens[1])
 
 if __name__ == '__main__':
