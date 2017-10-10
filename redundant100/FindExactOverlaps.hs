8b1f9c3ff7e7cb57b5ffbad7f98032b8d33679f7
Luis Pedro Coelho <luis@luispedro.org>
2017-10-11 00:03:56 +0200

    ENH Faster FindExactOverlaps
    The bottleneck was in the library call to the rolling hash functions
    These were implemented in a very naive way.
    Using the new approach, a simple test (1 million genes vs 1 million genes) were processed 5x faster

--- redundant100/FindExactOverlaps.hs
+++ redundant100/FindExactOverlaps.hs
@@ -14,6 +14,7 @@ import qualified Data.Conduit as C
 import           Data.Conduit ((.|))
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VUM
 import qualified Data.IntMap.Strict as IM
 import           Safe (headDef, atDef)
 import           System.IO.SafeWrite (withOutputFile)
@@ -25,8 +26,10 @@ import           Control.Applicative ((<|>))
 import Control.Monad
 import System.Console.GetOpt
 import Data.Maybe
+import Data.Bits
 import Data.List
 import Control.Monad.IO.Class
+import Control.Monad.ST
 import Control.Monad.Base
 import Data.Word
 import Data.Hash
@@ -35,6 +38,39 @@ import qualified Data.HashTable.IO as HT
 import Data.BioConduit
 import Data.Conduit.Algorithms.Utils
 import Data.Conduit.Algorithms.Async
+
+-- This is the same as Data.Hash.combine, but we cannot use it because it does
+-- not export the Hash constructor
+hashCombine :: Word64 -> Word64 -> Word64
+hashCombine a b = rotate a 1 `xor` b
+
+rollall :: Int -> B.ByteString -> [Int]
+rollall n bs
+    | n > B.length bs = []
+    | otherwise = runST $ do
+        hashes <- VUM.new n
+        let inithash ch p
+                | p == n - 1 = return (ch :: Word64)
+                | otherwise = do
+                    let hi = asWord64 $ hashWord8 (B.index bs p)
+                        ch' = ch `hashCombine` hi
+                    VUM.unsafeWrite hashes (p+1) hi
+                    inithash ch' (p+1)
+            initial_hash :: Word64
+            initial_hash = (asWord64 $ hash ()) `hashCombine` (asWord64 $ hash n)
+        VUM.unsafeWrite hashes 0 initial_hash
+        ch <- inithash initial_hash 0
+        let iter acc@(ch:_) p pix
+                | p == B.length bs = return . tail . map (fromIntegral . toInteger) $ reverse acc
+                | otherwise =  do
+                    let ck = asWord64 (hash $ B.index bs p)
+                    cl <- VUM.unsafeRead hashes pix
+                    let ch' :: Word64
+                        ch' = ch `hashCombine` (rotate cl n `xor` ck)
+                    VUM.unsafeWrite hashes pix ck
+                    iter (ch':acc) (p+1) (if pix + 1 == n then 0 else pix + 1)
+        iter [ch] (n-1) 0
+
 
 type FastaIOHash = HT.CuckooHashTable Int [Fasta]
 type FastaMap = IM.IntMap [Fasta]
@@ -56,21 +92,8 @@ printEvery10k = printEvery10k' (1 :: Int) (10000 :: Int)
                 C.yield v
                 printEvery10k' n (r - 1)
 
-allHashes :: Int -> Fasta -> [Int]
-allHashes hashSize (Fasta _ faseq) = toInt <$> hashrest (B.unpack cont) initH
-  where
-    (beg,cont) = B.splitAt hashSize faseq
-    initH :: RollingHash Word8
-    initH = B.foldl' addAndRoll (rollingHash hashSize) beg
-    hashrest [] rh = [currentHash rh]
-    hashrest (c:cs) rh = (currentHash rh:hashrest cs (addAndRoll rh c))
-    toInt :: Hash -> Int
-    toInt = fromIntegral . toInteger . asWord64
-
-
-
 annotate :: Int -> Fasta -> (Fasta, (VU.Vector Int))
-annotate hashSize fa = (fa, VU.fromList (allHashes hashSize fa))
+annotate hashSize fa@(Fasta _ fad) = (fa, VU.fromList (rollall hashSize fad))
 
 buildHash :: (MonadIO m, MonadBase IO m) => Int -> C.Sink Fasta m SizedHash
 buildHash nthreads = C.peek >>= \case
@@ -122,7 +145,9 @@ findOverlapsAcross n (SizedHash hashSize imap) = CC.conduitVector 4096 .| asyncM
         findOverlapsAcross'1 :: Fasta -> V.Vector B.ByteString
         findOverlapsAcross'1 faseq@(Fasta sid _) = V.fromList [B.concat [seqheader c, "\tC\t", sid] | c <- covered]
             where
-                hashes = allHashes hashSize faseq
+                -- Starting in containers 0.5.8, we could use the
+                -- IM.restrictKeys function to "lookup" all the keys at once
+                hashes = rollall hashSize (seqdata faseq)
                 candidates = flip concatMap hashes $ \h -> IM.findWithDefault [] h imap
                 covered = filter (`faContainedIn` faseq) candidates
 
